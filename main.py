4c9d11f88217114312ac807fbf7100910e2d0374
Mark ten Klooster <107471978+marktk24@users.noreply.github.com>
2023-03-20 14:21:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import csv
-print(dir(csv))
+# import psutil
+try:
+    import psutil
+except ImportError as e:
+    print("psutil is not installed.")
+    print("please install psutil.")
+    exit()
+
+adapters = psutil.net_if_addrs().items()
+
+for adapter in adapters:
+    print(adapter[0])
+
+    for info in adapter[1]:
+        if info[0] == 2:
+            print(info[1])
+    print("________________________")
+
